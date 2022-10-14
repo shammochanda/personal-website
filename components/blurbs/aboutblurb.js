dd3e6d8bd35def7a770cb5902e08b3d605ad44fa
shammochanda <shammochanda97@gmail.com>
2022-10-14 16:57:08 -0700

    change about

--- components/blurbs/aboutblurb.js
+++ components/blurbs/aboutblurb.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import Details from "../details";
-import Headings from "../headings"
+import Headings from "../headings";
 import classes from "./blurb.module.css";
 
 const AboutBlurb = (props) => {
@@ -8,10 +8,15 @@ const AboutBlurb = (props) => {
     <div className={classes.main}>
       <Headings>About Me</Headings>
       <Details>
-        I recently graduated from UCLA and am currently looking for a job as a frontend developer. I am 
-        familiar with the React framework, as well as associated frameworks such as Next.js and React Router. 
-        I believe in simple, yet eye-catching design that makes for an uncomplicated user experience. As a 
-        potential member of your team, I look forward to working closely with others to further a collective goal.
+        I recently graduated from UCLA and am currently looking for a job as a
+        software engineer. I am familiar with the React framework, as well as
+        associated frameworks such as Next.js and React Router. I believe in
+        simple, yet eye-catching design that makes for an uncomplicated user
+        experience. I am also extensively experienced in numerous backend
+        languages such as C++ and Python. My ability to analyse problems and
+        come up with well-constructed solutions makes me well-suited to tackle
+        any role. As a potential member of your team, I look forward to working
+        closely with others to further a collective goal.
       </Details>
     </div>
   );
